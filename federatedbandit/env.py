e2dd7856a247a999bacf1031cdf5a0ffdc1bd7e7
Jialin Yi <j.yi8@lse.ac.uk>
2022-12-30 23:58:49 +0000

    MovieLens dateset

--- federatedbandit/env.py
+++ federatedbandit/env.py
@@ -1,4 +1,7 @@
+import os
+import datetime
 import numpy as np
+import pandas as pd
 from torch.utils.data import Dataset
 
 class HomoBandit(Dataset):
@@ -38,3 +41,44 @@ class FixActBandit(HomoBandit):
         super().__init__(n_epochs, n_agents, n_arms, rng)
         self.data[:,activate_size:,:] = 0
             
+
+class MovieLens(Dataset):
+    def __init__(self) -> None:
+        super().__init__()
+        self.data = pd.read_pickle(
+            os.path.join(
+                os.path.dirname(__file__),
+                '../MovieLens/MovieLens_loss.pkl'
+            )
+        )
+        start = self.data.index.values[0][0]
+        end = self.data.index.values[-1][0]
+        self.start_date = datetime.datetime.strptime(start, '%Y-%m-%d').date()
+        self.end_date = datetime.datetime.strptime(end, '%Y-%m-%d').date()
+        self.genres = ['Action', 'Adventure', 'Animation', 'Children', 'Comedy', 'Crime',
+            'Drama', 'Fantasy', 'Film-Noir', 'Horror', 'IMAX', 'Musical',
+            'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western',
+            'Documentary', '(no genres listed)'
+        ]
+        delta = self.end_date - self.start_date
+        self.T = delta.days + 1
+        self.N = 610
+        self.K = len(self.genres)
+
+    def __len__(self):
+        return self.T
+
+    def get_armId(self, genre):
+        return self.genres.index(genre)
+    
+    def __getitem__(self, index):
+        today = self.start_date + datetime.timedelta(days=index)
+        losses = np.zeros((self.N, self.K))
+        for i in range(self.N):
+            for j, genre in enumerate(self.genres):
+                key = (str(today), i+1, genre)
+                if key in self.data.index:
+                    losses[i, j] = self.data['loss'][key]
+        return losses
+
+
