42c053ea2664a00388ef1a59d0a9e9217b1eca2c
Jialin Yi <j.yi8@lse.ac.uk>
2022-12-31 10:17:55 +0000

    MovieLens numpy

--- federatedbandit/env.py
+++ federatedbandit/env.py
@@ -1,9 +1,9 @@
-import os
-# import sys
+import os, sys
 import datetime
 import numpy as np
 import pandas as pd
 from torch.utils.data import Dataset
+from tqdm import tqdm
 
 class DoubleAdvBandit(Dataset):
     def __len__(self):
@@ -46,86 +46,103 @@ class FixActBandit(HomoBandit):
         self.data[:,activate_size:,:] = 0
             
 
-# class MovieLens(DoubleAdvBandit):
-#     def __init__(self) -> None:
-#         super().__init__()
-#         self.genres = ['Action', 'Adventure', 'Animation', 'Children', 'Comedy', 'Crime',
-#             'Drama', 'Fantasy', 'Film-Noir', 'Horror', 'IMAX', 'Musical',
-#             'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western',
-#             'Documentary', '(no genres listed)'
-#         ]
-#         start = '1996-03-29'
-#         end = '2018-09-24'
-#         self.start_date = datetime.datetime.strptime(start, '%Y-%m-%d').date()
-#         self.end_date = datetime.datetime.strptime(end, '%Y-%m-%d').date()
-#         delta = self.end_date - self.start_date
-#         self.n_epochs = delta.days + 1
-#         self.n_agents = 610
-#         self.n_arms = len(self.genres)
-#         try:
-#             with open('MovieLens.npy', 'rb') as f:
-#                 self.data = np.load(f)
-#         except:
-#             if query_yes_no("Do you want to generate the loss data for MovieLens? (Y/N): "):
-#                 pkldata = pd.read_pickle(
-#                     os.path.join(
-#                         os.path.dirname(__file__),
-#                         '../MovieLens/MovieLens_loss.pkl'
-#                     )
-#                 )
-#                 self.data = np.zeros(
-#                     (
-#                         self.n_epochs,
-#                         self.n_agents,
-#                         self.n_arms
-#                     ),
-#                     dtype=np.float32
-#                 )
-#                 for index in range(self.n_epochs):
-#                     today = self.start_date + datetime.timedelta(days=index)
-#                     for i in range(self.N):
-#                         for j, genre in enumerate(self.genres):
-#                             key = (str(today), i+1, genre)
-#                             if key in pkldata.index:
-#                                 self.data[index, i, j] = pkldata['loss'][key]
-#                 if query_yes_no("Do you want to save MovieLens_loss.npy? (Y/N): "):
-#                     dest = os.path.join(
-#                             os.path.dirname(__file__),
-#                             '../MovieLens/MovieLens_loss.npy'
-#                     )
-#                     with open(dest, 'wb') as f:
-#                         np.save(f, self.data)
-#                     print('Data saved: ' + dest)
+class MovieLens(DoubleAdvBandit):
+    def __init__(self) -> None:
+        super().__init__()
+        self.genres = ['Action', 'Adventure', 'Animation', 'Children', 'Comedy', 'Crime',
+            'Drama', 'Fantasy', 'Film-Noir', 'Horror', 'IMAX', 'Musical',
+            'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western',
+            'Documentary', '(no genres listed)'
+        ]
+        start = '1996-03-29'
+        end = '2018-09-24'
+        self.start_date = datetime.datetime.strptime(start, '%Y-%m-%d').date()
+        self.end_date = datetime.datetime.strptime(end, '%Y-%m-%d').date()
+        delta = self.end_date - self.start_date
+        self.n_epochs = delta.days + 1
+        self.n_agents = 610
+        self.n_arms = len(self.genres)
+        path_to_MovieLens = os.path.join(
+            os.path.dirname(__file__),
+            '../MovieLens/'
+        )
+        path_to_npy_data = os.path.join(
+                path_to_MovieLens,
+                'MovieLens_loss.npy'
+        )
+        try:
+            with open(path_to_npy_data, 'rb') as f:
+                self.data = np.load(f)
+        except FileNotFoundError:
+            if query_yes_no("Do you want to generate the loss data for MovieLens?"):
+                pkldata = pd.read_pickle(
+                    os.path.join(
+                        os.path.dirname(__file__),
+                        '../MovieLens/MovieLens_loss.pkl'
+                    )
+                )
+                self.data = np.zeros(
+                    (
+                        self.n_epochs,
+                        self.n_agents,
+                        self.n_arms
+                    ),
+                    dtype=np.float32
+                )
+                for index in tqdm(range(self.n_epochs)):
+                    today = self.start_date + datetime.timedelta(days=index)
+                    for i in range(self.n_agents):
+                        for j, genre in enumerate(self.genres):
+                            key = (str(today), i+1, genre)
+                            if key in pkldata.index:
+                                self.data[index, i, j] = pkldata['loss'][key]
+                if query_yes_no("Do you want to save MovieLens_loss.npy?"):
+                    with open(path_to_npy_data, 'wb') as f:
+                        np.save(f, self.data)
+                    print('Data saved: ' + path_to_npy_data)
+
+    def get_loss_by_key(self, key):
+        date_str, u_id, genre = key
+        date = datetime.datetime.strptime(date_str, '%Y-%m-%d').date()
+        delta = date - self.start_date
+        return self.data[delta.days, u_id-1, self.get_armId(genre)]
+
+    def get_armId(self, genre):
+        return self.genres.index(genre)
 
                 
 
 
-# def query_yes_no(question, default="yes"):
-#     """Ask a yes/no question via raw_input() and return their answer.
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
 
-#     "question" is a string that is presented to the user.
-#     "default" is the presumed answer if the user just hits <Enter>.
-#             It must be "yes" (the default), "no" or None (meaning
-#             an answer is required of the user).
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+            It must be "yes" (the default), "no" or None (meaning
+            an answer is required of the user).
 
-#     The "answer" return value is True for "yes" or False for "no".
-#     """
-#     valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
-#     if default is None:
-#         prompt = " [y/n] "
-#     elif default == "yes":
-#         prompt = " [Y/n] "
-#     elif default == "no":
-#         prompt = " [y/N] "
-#     else:
-#         raise ValueError("invalid default answer: '%s'" % default)
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
 
-#     while True:
-#         sys.stdout.write(question + prompt)
-#         choice = input().lower()
-#         if default is not None and choice == "":
-#             return valid[default]
-#         elif choice in valid:
-#             return valid[choice]
-#         else:
-#             sys.stdout.write("Please respond with 'yes' or 'no' " "(or 'y' or 'n').\n")
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == "":
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' " "(or 'y' or 'n').\n")
+
+
+if __name__ == '__main__':
+    train = MovieLens()
--- test/test_env.py
+++ test/test_env.py
@@ -1,4 +1,6 @@
+import os, pytest
 import numpy as np
+import pandas as pd
 import federatedbandit.env as fbe
 
 def test_stochastic_activation_bandit():
@@ -39,14 +41,29 @@ def test_least_cum_loss(n_epochs=1000, n_agents=16, n_arms=50, seed=0):
     assert np.isclose(res, 0, rtol=1e-05, atol=1e-08, equal_nan=False)
 
 
-# def test_movie_lens():
-#     data = fbe.MovieLens()
-#     assert len(data) == 8215
-#     res = data.__getitem__(0)[428, data.get_armId('Action')]
-#     assert np.isclose(res, 0.323529, rtol=1e-05, atol=1e-08, equal_nan=False)
-#     res = data.__getitem__(0)[428, data.get_armId('Animation')]
-#     assert np.isclose(res, 0.166667, rtol=1e-05, atol=1e-08, equal_nan=False)
-#     res = data.__getitem__(data.T-1)[513, data.get_armId('Action')]
-#     assert np.isclose(res, 0.700000, rtol=1e-05, atol=1e-08, equal_nan=False)
-#     res = data.__getitem__(data.T-1)[513, data.get_armId('Documentary')]
-#     assert np.isclose(res, 0.300000, rtol=1e-05, atol=1e-08, equal_nan=False)
+def test_movie_lens():
+    data = fbe.MovieLens()
+    assert len(data) == 8215
+    res = data.__getitem__(0)[428, data.get_armId('Action')]
+    assert np.isclose(res, 0.323529, rtol=1e-05, atol=1e-08, equal_nan=False)
+    res = data.__getitem__(0)[428, data.get_armId('Animation')]
+    assert np.isclose(res, 0.166667, rtol=1e-05, atol=1e-08, equal_nan=False)
+    res = data.__getitem__(data.n_epochs-1)[513, data.get_armId('Action')]
+    assert np.isclose(res, 0.700000, rtol=1e-05, atol=1e-08, equal_nan=False)
+    res = data.__getitem__(data.n_epochs-1)[513, data.get_armId('Documentary')]
+    assert np.isclose(res, 0.300000, rtol=1e-05, atol=1e-08, equal_nan=False)
+
+
+def test_movie_lens_random_access(excution_number = 10):
+    for _ in range(excution_number):
+        data = fbe.MovieLens()
+        gb = pd.read_pickle(
+            os.path.join(
+                os.path.dirname(__file__),
+                '../MovieLens/MovieLens_loss.pkl'
+            )
+        )
+        row = gb.sample(n=1)
+        res = data.get_loss_by_key(row.index.values[0])
+        ans = row['loss'][row.index.values[0]]
+        assert  np.isclose(res, ans, rtol=1e-05, atol=1e-08, equal_nan=False)
