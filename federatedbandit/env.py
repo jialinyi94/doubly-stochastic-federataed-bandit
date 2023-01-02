88e62dfcacad2e82b41bc6d37432dd6129980c81
Jialin Yi <j.yi8@lse.ac.uk>
2023-01-02 00:07:39 +0000

    MovieLens latest full

--- federatedbandit/env.py
+++ federatedbandit/env.py
@@ -44,112 +44,17 @@ class FixActBandit(HomoBandit):
         self.data[:,activate_size:,:] = 0
             
 
-class MovieLens(DoubleAdvBandit):
-    def __init__(self) -> None:
+class RealData(DoubleAdvBandit):
+    def __init__(self, file, rate_min, rate_max, genres) -> None:
         super().__init__()
-        path_to_MovieLens = os.path.join(
-            os.path.dirname(__file__),
-            '../MovieLens/'
-        )
-        path_to_npy_data = os.path.join(
-                path_to_MovieLens,
-                'MovieLens_loss.npy'
-        )
         try:
-            with open(path_to_npy_data, 'rb') as f:
+            with open(file, 'rb') as f:
                 self.data = np.load(f)
                 self.n_epochs, self.n_agents, self.n_arms = self.data.shape
-                self.genres = ['Action',
-                    'Adventure',
-                    'Animation',
-                    'Children',
-                    'Comedy',
-                    'Crime',
-                    'Drama',
-                    'Fantasy',
-                    'Film-Noir',
-                    'Horror',
-                    'Musical',
-                    'Mystery',
-                    'Romance',
-                    'Sci-Fi',
-                    'Thriller',
-                    'War',
-                    'Western',
-                    'Documentary',
-                    'IMAX',
-                    '(no genres listed)'
-                ]
+                self.genres = genres
+                self.r_min, self.r_max = rate_min, rate_max
         except FileNotFoundError:
-            if query_yes_no("Do you want to generate the loss data for MovieLens?"):
-                movies_df = pd.read_csv(
-                    os.path.join(
-                        path_to_MovieLens,
-                        'movies.csv'
-                    )
-                )
-                ratings_df = pd.read_csv(
-                    os.path.join(
-                        path_to_MovieLens,
-                        'ratings.csv'
-                    )
-                )
-                ratings_df = pd.merge(
-                    ratings_df,
-                    movies_df,
-                    on = 'movieId'
-                )
-                ratings_df['genres'] = ratings_df['genres'].apply(
-                    lambda row: row.split('|')
-                )
-                ratings_df = ratings_df.explode('genres').sort_values(['timestamp', 'userId', 'genres'])
-                ratings_gb = ratings_df.groupby(['userId', 'genres'])['rating'].aggregate(list).reset_index()
-                ratings_gb['rating'] = ratings_gb['rating'].apply(
-                    lambda row: [(v, i) for i, v in enumerate(row)]
-                )
-                ratings_gb = ratings_gb.explode('rating')
-                ratings_gb['epoch'] = ratings_gb['rating'].apply(
-                    lambda row: row[-1]
-                )
-                ratings_gb['rating'] = ratings_gb['rating'].apply(
-                    lambda row: row[0]
-                )
-                ratings_gb.reset_index(inplace=True)
-                all_df = pd.DataFrame(
-                    [{
-                        'userId': list(range(1, ratings_gb['userId'].max()+1)),
-                        'genres': list(ratings_gb['genres'].unique()),
-                        'epoch': list(range(ratings_gb['epoch'].max()+1))
-                    }]
-                )
-                all_df = all_df.explode(['epoch'])
-                all_df = all_df.explode(['userId'])
-                all_df = all_df.explode(['genres'])
-                all_df = pd.merge(
-                    all_df,
-                    ratings_gb,
-                    how='left',
-                    on = ['userId', 'genres', 'epoch']
-                )
-                all_df.fillna(
-                    value = (ratings_df['rating'].max() + ratings_df['rating'].min()) / 2,
-                    inplace = True
-                )
-                ratings = all_df['rating'].to_numpy(dtype=np.float32)
-                losses = (ratings.max() + ratings.min() - ratings) / ratings.max()
-                losses = losses.reshape(
-                    ratings_gb['epoch'].max()+1,
-                    ratings_gb['userId'].max(),
-                    len(ratings_gb['genres'].unique()),
-                    order='C'
-                )
-                self.data = losses
-                self.n_epochs, self.n_agents, self.n_arms = losses.shape
-                self.genres = all_df.head(20)['genres'].values.tolist()
-                if query_yes_no("Do you want to save MovieLens_loss.npy?"):
-                    with open(path_to_npy_data, 'wb') as f:
-                        np.save(f, self.data)
-                    print('Data saved: ' + path_to_npy_data)
+            print('Please follow the colab notebook to download the dataset.')
 
     def get_armId(self, genre):
         return self.genres.index(genre)
@@ -157,39 +62,53 @@ class MovieLens(DoubleAdvBandit):
     def get_genre(self, arm_id):
         return self.genres[arm_id]
 
-    def rating_to_loss(self, rating, r_max=5, r_min=.5):
-        return (r_max + r_min - rating) / r_max
+    def rate2loss(self, r):
+        return (self.r_max + self.r_min - r) / self.r_max
 
 
-def query_yes_no(question, default="yes"):
-    """Ask a yes/no question via raw_input() and return their answer.
+class MovieLens(RealData):
+    def __init__(self) -> None:
+        super().__init__(
+            file=os.path.join(
+                os.path.dirname(__file__),
+                '../MovieLens/movielens.npy'
+            ),
+            rate_min=.5, 
+            rate_max=5, 
+            genres = ['(no genres listed)',
+                'Action',
+                'Adventure',
+                'Animation',
+                'Children',
+                'Comedy',
+                'Crime',
+                'Documentary',
+                'Drama',
+                'Fantasy',
+                'Film-Noir',
+                'Horror',
+                'IMAX',
+                'Musical',
+                'Mystery',
+                'Romance',
+                'Sci-Fi',
+                'Thriller',
+                'War',
+                'Western'
+            ]
+        )
 
-    "question" is a string that is presented to the user.
-    "default" is the presumed answer if the user just hits <Enter>.
-            It must be "yes" (the default), "no" or None (meaning
-            an answer is required of the user).
-
-    The "answer" return value is True for "yes" or False for "no".
-    """
-    valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
-    if default is None:
-        prompt = " [y/n] "
-    elif default == "yes":
-        prompt = " [Y/n] "
-    elif default == "no":
-        prompt = " [y/N] "
-    else:
-        raise ValueError("invalid default answer: '%s'" % default)
-
-    while True:
-        sys.stdout.write(question + prompt)
-        choice = input().lower()
-        if default is not None and choice == "":
-            return valid[default]
-        elif choice in valid:
-            return valid[choice]
-        else:
-            sys.stdout.write("Please respond with 'yes' or 'no' " "(or 'y' or 'n').\n")
+# class AmazonReview(RealData):
+#     def __init__(self) -> None:
+#         super().__init__(
+#             file=os.path.join(
+#                 os.path.dirname(__file__),
+#                 '../Amazon/amazonreviews.npy'
+#             ),
+#             rate_min=.5, 
+#             rate_max=5, 
+#             genres = []
+#         )
 
 
 if __name__ == '__main__':
--- test/test_env.py
+++ test/test_env.py
@@ -44,67 +44,16 @@ def test_least_cum_loss(n_epochs=1000, n_agents=16, n_arms=50, seed=0):
 
 def test_movie_lens():
     movie_lens = fbe.MovieLens()
-    assert len(movie_lens) == 1309
-    assert movie_lens.__getitem__(0).shape == (610, 20)
+    assert len(movie_lens) == 12800
+    assert movie_lens.__getitem__(0).shape == (58**2, 20)
     epoch = 4
-    i = 1 - 1
+    i = 0
     k = movie_lens.get_armId('Action')
     res = movie_lens.data[epoch, i, k] 
-    ans = movie_lens.rating_to_loss(4.0)
+    ans = movie_lens.rate2loss(4.5)
     assert np.isclose(res, ans, rtol=1e-05, atol=1e-08, equal_nan=False)
-    fitted_loss = movie_lens.rating_to_loss(2.75)
-    assert len(np.where(movie_lens.data != fitted_loss)[0]) == 274480
-
-
-def test_movie_lens_random_access(excution_number = 10):
-    movie_lens = fbe.MovieLens()
-    path_to_MovieLens = os.path.join(
-        os.path.dirname(__file__),
-        '../MovieLens/'
-    )
-    movies_df = pd.read_csv(
-        os.path.join(
-            path_to_MovieLens,
-            'movies.csv'
-        )
-    )
-    ratings_df = pd.read_csv(
-        os.path.join(
-            path_to_MovieLens,
-            'ratings.csv'
-        )
-    )
-    ratings_df = pd.merge(
-        ratings_df,
-        movies_df,
-        on = 'movieId'
-    )
-    ratings_df['genres'] = ratings_df['genres'].apply(
-        lambda row: row.split('|')
-    )
-    ratings_df = ratings_df.explode('genres').sort_values(['timestamp', 'userId', 'genres'])
-    ratings_gb = ratings_df.groupby(['userId', 'genres'])['rating'].aggregate(list).reset_index()
-    ratings_gb['rating'] = ratings_gb['rating'].apply(
-        lambda row: [(v, i) for i, v in enumerate(row)]
-    )
-    ratings_gb = ratings_gb.explode('rating')
-    ratings_gb['epoch'] = ratings_gb['rating'].apply(
-        lambda row: row[-1]
-    )
-    ratings_gb['rating'] = ratings_gb['rating'].apply(
-        lambda row: row[0]
-    )
-    ratings_gb.reset_index(inplace=True)
-    for _ in range(excution_number):
-        idx = np.random.randint(0, ratings_gb.shape[0])
-        epoch = ratings_gb['epoch'][idx]
-        i = ratings_gb['userId'][idx] - 1
-        k = movie_lens.get_armId(ratings_gb['genres'][idx])
-        res = movie_lens.data[epoch, i, k]
-        ans = movie_lens.rating_to_loss(ratings_gb['rating'][idx])
-        assert np.isclose(res, ans, rtol=1e-05, atol=1e-08, equal_nan=False)
 
 def test_movie_lens_best_arm():
     movie_lens = fbe.MovieLens()
     cum_losses, arm_id = movie_lens.cumloss_of_best_arm()
-    assert movie_lens.get_genre(arm_id) == 'Drama'
+    assert movie_lens.get_genre(arm_id) == 'Film-Noir'
